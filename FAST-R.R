e631f47da378a44338fbf7113cf6d25f3797e3c2
fneri <f.francesconeri@gmail.com>
2023-10-10 11:08:33 -0700

    added install.packages("rgl")

--- FAST-R.R
+++ FAST-R.R
@@ -5,20 +5,20 @@
 # Note that you'll need to remove the "#" in front of all of the install.packages lines (e.g. "install.packages("shiny")") if
 # this is the first time you're using this script (and have never installed the required packages before).
 
-# install.packages("shiny")
-# install.packages("devtools")
-# install.packages("remotes")
-# remotes::install_github('wleepang/shiny-directory-input')
-# install.packages("tidyverse")
-# install.packages("readxl")
-# install.packages("plater")
-# install.packages("beepr")
-# install.packages("openxlsx")
-# install.packages("scales")
-# remotes::install_github("dmurdoch/rgl")
-# install.packages("htmlwidgets")
+ # install.packages("shiny")
+ # install.packages("devtools")
+ # install.packages("remotes")
+ # remotes::install_github('wleepang/shiny-directory-input')
+ # install.packages("tidyverse")
+ # install.packages("readxl")
+ # install.packages("plater")
+ # install.packages("beepr")
+ # install.packages("openxlsx")
+ # install.packages("scales")
+ # install.packages("rgl")
+ # remotes::install_github("dmurdoch/rgl")
+ # install.packages("htmlwidgets")
 
 
 library(shiny)
 runGitHub("FAST-Data-Analysis", "f-neri")
-
